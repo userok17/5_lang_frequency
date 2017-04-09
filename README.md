290e3614d6bf9bb3b173acef7de6c3f9dce614ae
userok <admin@1rpi.ru>
2017-04-09 18:07:25 +0500

    Использование библиотеки optparse

--- README.md
+++ README.md
@@ -4,4 +4,41 @@
 
 Скрипт должен принимать на вход путь до текстового файла и выводить в консоль десять самых популярных слов в этом файле в порядке убывания частоты.
 
+
+# Quickstart
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+
+
+```#!bash
+
+$ python lang_frequency.py -h
+Usage: lang_frequency.py [options]
+
+Options:
+  -h, --help            show this help message and exit
+  -f FILEPATH, --filepath=FILEPATH
+                        Укажите путь до файла
+
+$ python lang_frequency.py -f <path to file>
+
+Десять самых популярных слов в этом файле:
+в          20
+-          9
+на         7
+и          6
+г.         5
+с          5
+–          4
+2015       3
+не         3
+тысяч      3
+
+```
+
+# Project Goals
+
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,5 +1,6 @@
 import os
 from collections import Counter
+from optparse import OptionParser
 
 def load_data(filepath):
     if not os.path.exists(filepath):
@@ -11,11 +12,24 @@ def load_data(filepath):
 def get_most_frequent_words(text):
     words = text.split()
     counts =  Counter(words)
-    return counts.most_common(10)
+    count_rows = 10
+    return counts.most_common(count_rows)
 
+def print_most_frequent_words(counts):
+    print('Десять самых популярных слов в этом файле:')
+    for key, value in counts:
+        print('{:10} {}'.format(key, value))
+
+def parser_command():
+    parser = OptionParser()
+    parser.add_option('-f', '--filepath', help='Укажите путь до файла',dest='filepath')
+    opts, args = parser.parse_args()
+    if opts.filepath:
+        text = load_data(opts.filepath)
+        if text:
+            count_words = get_most_frequent_words(text)
+            print_most_frequent_words(count_words)
+    
 
 if __name__ == '__main__':
-    text = load_data('text.txt')
-    count_words = get_most_frequent_words(text)
-    for key, value in count_words:
-        print('{:10} {}'.format(key, value))
+    parser_command()
