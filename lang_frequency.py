38f017ae6f9bc7130d8444092b1a90597beec492
userok <admin@1rpi.ru>
2017-04-12 03:55:06 +0500

    исключение слов и весь список в нижний регистр
    1. Исключает если это не слово. Все слова в нижний регистр. Тут я не совсем понял, где нужно менять значение в двух местах?

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,7 @@
 import os
 from collections import Counter
-from optparse import OptionParser
+from argparse import ArgumentParser
+import re
 
 def load_data(filepath):
     if not os.path.exists(filepath):
@@ -9,27 +10,38 @@ def load_data(filepath):
         return data_file.read()
 
 
-def get_most_frequent_words(text):
-    words = text.split()
-    counts =  Counter(words)
-    count_rows = 10
-    return counts.most_common(count_rows)
+def exclude_non_words(text):
+    regex_non_words = re.compile(r'([\d\W]+)')
+    text = regex_non_words.sub(r' ', text)
+    return text
 
-def print_most_frequent_words(counts):
-    print('Десять самых популярных слов в этом файле:')
-    for key, value in counts:
+def get_most_frequent_words(text, rows):
+    text = exclude_non_words(text) # Исключить из текста, символы, которые не являются словами
+    list_words = text.split()
+    list_words = list(map(lambda word: word.lower(), list_words)) # Все слова в нижний регистр
+    counts =  Counter(list_words)
+    return counts.most_common(rows)
+
+
+def main():
+    parser = ArgumentParser(description='Чистота слов')
+    parser.add_argument('-f', '--filepath', required=True, help='Укажите путь до файла',dest='filepath')
+    args = parser.parse_args()
+
+    text = load_data(args.filepath)
+    if not text:
+        print('Неверно указан путь до текстового файла')
+        sys.exit(1)
+
+    rows = 10
+
+    description = '{} самых популярных слов в этом файле:'.format(rows)
+    print(description)
+    
+    count_words = get_most_frequent_words(text, rows)
+
+    for key, value in count_words:
         print('{:10} {}'.format(key, value))
 
-def parser_command():
-    parser = OptionParser()
-    parser.add_option('-f', '--filepath', help='Укажите путь до файла',dest='filepath')
-    opts, args = parser.parse_args()
-    if opts.filepath:
-        text = load_data(opts.filepath)
-        if text:
-            count_words = get_most_frequent_words(text)
-            print_most_frequent_words(count_words)
-    
-
 if __name__ == '__main__':
-    parser_command()
+    main()
